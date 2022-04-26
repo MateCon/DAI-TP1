931b067d9eabaa3c4e9592d7ff1ff36350e94616
MateCon <mateoziffer@gmail.com>
2022-04-26 09:03:25 -0300

    get by id

--- src/controllers/personajeController.ts
+++ src/controllers/personajeController.ts
@@ -1,15 +1,37 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
 import { Personaje } from "../models/personaje";
-import { getAll } from "../services/personajeService";
+import { getAll, getById } from "../services/personajeService";
+import { IRecordSet } from "mssql";
 
 const router = Router();
 const jsonParser = bodyParser.json();
 
 router.get("/", async (req, res) => {
-	const data = await getAll();
+	try {
+		const personajes: IRecordSet<Personaje> = await getAll();
+		res.status(200).json(personajes);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("server error");
+	}
+});
 
-	res.json(data);
+router.get("/:id", async (req, res) => {
+	try {
+		const id = parseInt(req.params.id);
+
+		if (isNaN(id)) {
+			res.status(400).send("id is not a number");
+			return;
+		}
+
+		const personaje: IRecordSet<Personaje> = await getById(id);
+		res.status(200).json(personaje);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("server error");
+	}
 });
 
 export default router;
--- src/services/personajeService.ts
+++ src/services/personajeService.ts
@@ -1,9 +1,17 @@
-import { IRecordSet } from "mssql";
+import sql, { IRecordSet } from "mssql";
 import connect from "../utils/database";
 import { Personaje } from "../models/personaje";
 
 export const getAll = async (): Promise<IRecordSet<Personaje>> => {
 	const request = await connect();
 	const response = await request.query(`exec getAll`);
+	return response.recordset;
+};
+
+export const getById = async (id: number): Promise<IRecordSet<Personaje>> => {
+	const request = await connect();
+	const response = await request
+		.input("input_parameter", sql.Int, id)
+		.query(`exec getAll`);
 	return response.recordset;
 };
