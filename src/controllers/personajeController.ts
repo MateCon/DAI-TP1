18d82223882b6b0878a24feac1152d0b701471c2
FAUSTO MIR <46437709@ort>
2022-04-27 08:16:08 -0300

    add create update delete

--- src/controllers/personajeController.ts
+++ src/controllers/personajeController.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
 import { Personaje } from "../models/personaje";
-import { getAll, getById } from "../services/personajeService";
+import { create, deleteById, getAll, getById, update } from "../services/personajeService";
 import { IRecordSet } from "mssql";
 
 const router = Router();
@@ -28,6 +28,54 @@ router.get("/:id", async (req, res) => {
 
 		const personaje: IRecordSet<Personaje> = await getById(id);
 		res.status(200).json(personaje);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("server error");
+	}
+});
+
+router.post("/", async (req, res) => {
+	try {
+		const personaje: Personaje = req.body;
+
+		// Hay que checkear rowsAffeced?
+		await create(personaje);
+
+		res.status(200).send("personaje creado");
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("server error");
+	}
+});
+
+router.put("/", async (req, res) => {
+	try {
+		const personaje: Personaje = req.body;
+
+		// Hay que checkear rowsAffeced? x2
+		await update(personaje);
+
+		res.status(200).send("personaje actualizado");
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("server error");
+	}
+});
+
+
+router.delete("/:id", async (req, res) => {
+	try {
+		const id = parseInt(req.params.id);
+
+		if (isNaN(id)) {
+			res.status(400).send("id is not a number");
+			return;
+		}
+
+		// Hay que checkear rowsAffeced? x3 :o
+		await deleteById(id);
+
+		res.status(200).send("personaje eliminado");
 	} catch (err) {
 		console.log(err);
 		res.status(500).send("server error");
--- src/services/personajeService.ts
+++ src/services/personajeService.ts
@@ -4,7 +4,8 @@ import { Personaje } from "../models/personaje";
 
 export const getAll = async (): Promise<IRecordSet<Personaje>> => {
 	const request = await connect();
-	const response = await request.query(`exec getAll`);
+	const response = await request
+		.execute(`exec getAll`);
 	return response.recordset;
 };
 
@@ -12,6 +13,40 @@ export const getById = async (id: number): Promise<IRecordSet<Personaje>> => {
 	const request = await connect();
 	const response = await request
 		.input("input_parameter", sql.Int, id)
-		.query(`exec getAll`);
+		.execute(`exec getAll`);
 	return response.recordset;
+};
+
+export const create = async (personaje: Personaje): Promise<number[]> => {
+	const request = await connect();
+	const response = await request
+		.input("nombre", sql.VarChar(255), personaje.nombre ?? "")
+		.input("imagen", sql.VarChar(255), personaje.imagen ?? "")
+		.input("edad", sql.Int, personaje.edad ?? 0)
+		.input("peso", sql.Int, personaje.peso ?? 0)
+		.input("historia", sql.VarChar(600), personaje.historia ?? "")
+		.execute(`exec create @nombre, @imagen, @edad, @peso, @historia`);
+	return response.rowsAffected;
+};
+
+export const update = async (personaje: Personaje): Promise<number[]> => {
+	if (!personaje.id) throw new Error("Id es requerida");
+	const request = await connect();
+	const response = await request
+		.input("id", sql.Int, personaje.id)
+		.input("nombre", sql.VarChar(255), personaje.nombre ?? "")
+		.input("imagen", sql.VarChar(255), personaje.imagen ?? "")
+		.input("edad", sql.Int, personaje.edad ?? 0)
+		.input("peso", sql.Int, personaje.peso ?? 0)
+		.input("historia", sql.VarChar(600), personaje.historia ?? "")
+		.execute(`exec update @id, @nombre, @imagen, @edad, @peso, @historia`);
+	return response.rowsAffected;
+};
+
+export const deleteById = async (id: number): Promise<number[]> => {
+	const request = await connect();
+	const response = await request
+		.input("id", sql.Int, id)
+		.execute(`exec delete @id`);
+	return response.rowsAffected;
 };
