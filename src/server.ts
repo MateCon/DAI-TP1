04c831587e1aadc4950d8f77c37c18ca6e3849de
MateCon <mateoziffer@gmail.com>
2022-06-15 10:10:06 -0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,15 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import router from "./utils/router";
 import config from "./utils/database";
+import swaggerUi from "swagger-ui-express";
+import CharacterController from "./controllers/characterController";
+import FilmController from "./controllers/filmController";
+import AuthController from "./controllers/authController";
+const swaggerFile = require('../swagger_output.json');
 
 (async () => {
 	try {
-		const app: Application = router(express());
+		const app: Application = express();
 		const port = 8080 || process.env.PORT;
 
 		const appPool = new ConnectionPool(config);
@@ -16,6 +21,12 @@ import config from "./utils/database";
 		app.use(cors());
 		app.use(express.json());
 		app.use(bodyParser.json());
+
+		app.use("character", CharacterController);
+		app.use("film", FilmController);
+		app.use("auth", AuthController);
+		
+		app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 		app.get("*", (_, res) => {
 			res.status(404).send("404 error - page not found");
