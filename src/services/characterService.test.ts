e181adcce81cfa060a2fd3026549e242ba7c3344
matecon <mateoziffer@gmail.com>
2022-06-28 08:48:45 -0300

    tests for character service

--- src/services/characterService.test.ts
+++ src/services/characterService.test.ts
@@ -2,17 +2,50 @@ import { ConnectionPool } from "mssql";
 import * as characterService from "./characterService";
 import config from "../utils/database";
 
-test('get all characters', async () => {
-	const appPool = new ConnectionPool(config);
-    const response = await characterService.getAll(appPool);
-    expect(response).toBeTruthy();
-});
-
-test('create character', async () => {
-	const appPool = new ConnectionPool(config);
-    const response = await characterService.create(appPool, {
-        nombre: "Mate",
-        edad: 17
+describe('character service', () => {
+    it('gets all', async () => {
+        const appPool = await new ConnectionPool(config).connect();
+        const response = await characterService.getAll(appPool);
+        expect(response).toBeTruthy();
     });
-    expect(response).toBe(1);
+
+    it('creates', async () => {
+        const appPool = await new ConnectionPool(config).connect();
+        const response = await characterService.create(appPool, {
+            nombre: "Mate",
+            edad: 17
+        });
+        expect(response).toBe(1);
+    });
+
+    it('gets by id', async () => {
+        const appPool = await new ConnectionPool(config).connect();
+        const response = await characterService.getById(appPool, 1);
+        expect(response).toBeTruthy();
+        expect(response.edad).toBeDefined();
+    });
+
+    it('gets by filter', async () => {
+        const appPool = await new ConnectionPool(config).connect();
+        const response = await characterService.getAllWithFilter(appPool, {
+            nombres: ["Mate"],
+            edades: [],
+            pesos: []
+        });
+        expect(response).toBeTruthy();
+    });
+
+    it('updates', async () => {
+        const appPool = await new ConnectionPool(config).connect();
+        await characterService.update(appPool, { id: 1, nombre: "Fausto" })
+        const response = await characterService.getById(appPool, 1);
+        expect(response).toBeTruthy();
+        expect(response.nombre).toEqual("Fausto");
+    });
+
+    it('deletes', async () => {
+        const appPool = await new ConnectionPool(config).connect();
+        const response = await characterService.deleteById(appPool, 1);
+        expect(response).toBe(1);
+    });
 });
